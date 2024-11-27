7d699e77a144873c02fc8e77a14446651a09cadc
sumitengi <=>
2024-11-27 11:20:53 +0530

    practice of basic Javascript

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+var sname = "raju"
+var snum = 40
+
+function hello(){
+    var uname = "sumit"
+    var unum = 20
+
+    console.log(uname)
+    console.log(unum)
+    console.log(sname)
+}
+
+
+function hello2(){
+    var uname = "sandeep"
+    var unum = 21
+    
+    console.log(sname)
+    console.log(uname)
+    console.log(unum)
+}
